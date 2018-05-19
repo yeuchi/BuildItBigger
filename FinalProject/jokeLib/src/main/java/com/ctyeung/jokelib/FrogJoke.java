22a369c37d5774d628a2e8d7b7a1847284613325
yeuchi@gmail.com <yeuchi@gmail.com>
2018-05-19 18:50:15 -0500

    restart at step 0
    Oops… there is instruction and starter code !
    
    1. replace with starter code.
    2. add joke library in android app.

---
+++ FinalProject/jokeLib/src/main/java/com/ctyeung/jokelib/FrogJoke.java
@@ -0,0 +1,11 @@
+package com.ctyeung.jokelib;
+
+/**
+ * Created by ctyeung on 5/19/18.
+ */
+
+public class FrogJoke {
+    public String getJoke() {
+        return "When does a frog celebrates birthday ?\n\n\n...on leap year ";
+    }
+}
